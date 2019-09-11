7501749552a404ab3f358536ff5d020065331b92
Long Nguyễn <lyokha113@gmail.com>
2019-09-11 18:37:50 +0700

    Init project

---
+++ src/main/java/fpt/capstone/etbs/EtbsApplication.java
@@ -0,0 +1,19 @@
+package fpt.capstone.etbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EtbsApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtbsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EtbsApplication.class);
+    }
+}
---
+++ src/test/java/fpt/capstone/etbs/EtbsApplicationTests.java
@@ -0,0 +1,16 @@
+package fpt.capstone.etbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtbsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
