ffc0302cf51c329e73442f0c833f7eeea140bda5
Tran Duc Thai <dthai9323@gmail.com>
2019-11-19 14:46:43 +0700

    model attribute -> request body

--- src/main/java/fpt/capstone/etbs/controller/RawTemplateController.java
+++ src/main/java/fpt/capstone/etbs/controller/RawTemplateController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,7 +63,7 @@ public class RawTemplateController {
   @PutMapping("/raw/{id}")
   private ResponseEntity<ApiResponse> updateRawTemplate(
       @PathVariable("id") Integer id,
-      @Valid @ModelAttribute RawTemplateUpdateRequest request)
+      @Valid @RequestBody RawTemplateUpdateRequest request)
       throws Exception {
     Authentication auth = authenticationFacade.getAuthentication();
     UserPrincipal userPrincipal = (UserPrincipal) auth.getPrincipal();
